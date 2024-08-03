1000991381ab73b932145514f7b0b86229f13fec
ShahabGit85 <shahabqw4hdqw4hd.com>
2024-08-03 12:47:43 +0500

    make the setup for the dashboard

---
+++ src/app/dashboard/home/page.jsx
@@ -0,0 +1,13 @@
+"use client"
+import Layout from '../page'
+
+const Home = () => {
+
+  return (
+    <Layout>
+      <h1 className='text-center text-xl font-bold text-black '>This is Home page</h1>
+    </Layout>
+  )
+}
+
+export default Home
